7f5867636e0a5bde13ac216a684c4039bbb45906
Hiral <valahiral563@gmail.com>
2024-03-15 10:00:26 +0530

    Business section is done

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Button = () => {
+const Button = ({styles}) => {
   return (
-    <div>
-      Button
-    </div>
+    <button className={`bg-blue-gradient py-4 px-6 font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}>
+      Get stated
+    </button>
   )
 }
 
