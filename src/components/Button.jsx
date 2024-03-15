e037a11087dbf9410b9ad1b75a43fd1b24b676fe
Hiral <valahiral563@gmail.com>
2024-03-15 11:15:46 +0530

    cardDeal part is done

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({styles}) => {
   return (
-    <button className={`bg-blue-gradient py-4 px-6 font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}>
+    <button className={`bg-blue-gradient py-4 px-6 font-poppins font-medium text-[18px] text-primary outline-none ${styles} rounded-[10px]`}>
       Get stated
     </button>
   )
